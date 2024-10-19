c60d8db96a2a4b6d25b7ee5b95b4b3315498caaa
xia-xx-cv <49708396+xia-xx-cv@users.noreply.github.com>
2024-10-19 15:36:26 +0800

    test.py

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from torch.utils.data import ConcatDataset
-
-import os
 import numpy as np
 
 np.set_printoptions(linewidth=100)
@@ -11,15 +8,11 @@ import torch
 # import torch.backends.cudnn as cudnn
 import torch.nn as nn
 from torchvision import datasets, models, transforms
-
-# from transform.transforms_group import *
 from torch.utils.data import DataLoader
 import argparse
-from tensorboardX import SummaryWriter
 import warnings
-# from sklearn.metrics import f1_score
-# import torchvision.utils as t_utils
-# from sklearn.metrics import confusion_matrix, classification_report, multilabel_confusion_matrix
+import importlib
+from sklearn.metrics import confusion_matrix, classification_report, multilabel_confusion_matrix
 # from sklearn.metrics import f1_score
 # from sklearn.metrics import average_precision_score
 # from sklearn.metrics import roc_auc_score, cohen_kappa_score
@@ -28,24 +21,11 @@ warnings.filterwarnings('ignore')
 
 from config._data.datasetConf import EDDFS_delMandN_mc_conf, EDDFS_amd_conf, \
     EDDFS_dr_conf
-from config._data.datasetConf import EDDFS_glaucoma_conf, EDDFS_myopia_conf, EDDFS_rvo_conf, EDDFS_ls_conf, \
-    EDDFS_hyper_conf, EDDFS_other_conf, EDDFS_delN_ml_conf
-from config._data.datasetConf import ODIR_delMandN_mc_conf
-from config._data.datasetConf import APTOS2019_conf
+from config._data.datasetConf import *
 from models import resnet18
 from tools.eval_model import eval_model
 import matplotlib.pyplot as plt
 
-
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 def plot_confusion_matrix(cm, classes, savename, title='Confusion Matrix'):
     plt.figure(figsize=(12, 8), dpi=100)
@@ -59,7 +39,7 @@ def plot_confusion_matrix(cm, classes, savename, title='Confusion Matrix'):
         if c > 0.001:
             plt.text(x_val, y_val, "%0.2f" % (c,), color='red', fontsize=15, va='center', ha='center')
 
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.binary)
+    plt.imshow(cm, interpolation='nearest', cmap=plt.colormaps['binary'])   # plt.cm.binary in some versions
     plt.title(title)
     plt.colorbar()
     xlocations = np.array(range(len(classes)))
@@ -81,54 +61,37 @@ def plot_confusion_matrix(cm, classes, savename, title='Confusion Matrix'):
     plt.savefig(savename, format='png')
     plt.show()
 
+def main(args):
+    # configuring dataset
+    dataset_map = {
+        "EDDFS_delN_ml": EDDFS_delN_ml_conf,
+        "EDDFS_delMandN_mc": EDDFS_delMandN_mc_conf,
+        "EDDFS_amd": EDDFS_amd_conf,
+        'EDDFS_dr': EDDFS_dr_conf,
+        'EDDFS_glaucoma': EDDFS_glaucoma_conf,
+        'EDDFS_myopia': EDDFS_myopia_conf,
+        'EDDFS_rvo': EDDFS_rvo_conf,  # binary classification,
+        'EDDFS_ls': EDDFS_ls_conf,
+        'EDDFS_hyper': EDDFS_hyper_conf,
+        'EDDFS_other': EDDFS_other_conf,  # but we marked them as multi_classes for simplicity
+        'ODIR_delMandN_mc': ODIR_delMandN_mc_conf,
+        'APTOS2019': APTOS2019_conf,
+    }
+    dataset_module = importlib.import_module("datasets")  # Updated for simplicity and safety
+    dataset = getattr(dataset_module, args.dataset + "_Dataset")
+    opt_dataset = dataset_map[args.dataset]()  # Updated for simplicity and safety
+    classes_num = opt_dataset.classes_num
+    classes_names = opt_dataset.classes_names
+    image_root = opt_dataset.IMG_ROOT
+    label_dir = opt_dataset.LABEL_DIR
+    image_size = args.imagesize
 
-if __name__ == "__main__":
-    all_data_infor = ('EDDFS_delN_ml,'  # multi-label(ml) multi-disease without normal(delN)
-                      'EDDFS_delMandN_mc,'  # single-label multi-disease(mc) without normal(delMandN, 
-                                            # deleted multi-label and normal samples)
-                      'EDDFS_dr, EDDFS_amd,'  # DR and AMD grading
-                      'EDDFS_glaucoma, EDDFS_myopia, EDDFS_rvo,' # binary classification, 
-                      'EDDFS_ls, EDDFS_hyper, EDDFS_other,'  # but we marked them as multi_classes for simplicity
-                      'ODIR_delMandN_mc, APTOS2019')  # a multi-disease dataset ODIR, and a DR dataset APTOS
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--useGPU', type=int, default=0,
-                        help='-1: "cpu"; 0, 1, ...: "cuda:x";')
-    parser.add_argument('--seed', type=int, default=2022)
-
-    parser.add_argument('--dataset', type=str, default="ODIR_delMandN_mc",
-                        help=all_data_infor)
-    parser.add_argument('--preprocess', type=str, default='7',
-                        help='preprocessing type')
-    parser.add_argument('--imagesize', type=int, default=448,
-                        help='image size')
-
-    parser.add_argument('--net', type=str, default='coattnet_v2_withWeighted_tiny',
-                        help='coattnet_v2_withWeighted_tiny' # ours
-                             'resnet18,'
-                             'resnext50_32x4d,'
-                             'densenet121,'
-                             'inception_v3,'
-                             'efficientnet_b2, efficientnetv2_s,'
-                             'dnn_18,')
-
-    parser.add_argument('--numworkers', type=int, default=4,  # 0 might be suitable for windows
-                        help='num_workers')
-    parser.add_argument('--weight', type=str,
-                        default="weights/NC_delMandN_mc_7_448-parallelnet_v2_withWeighted_tiny_e51_bFalse_bs32-l9e-05_0.2-preFalse-lossce.pth.tar",
-                        help='load trained model?')
-    parser.add_argument('--lossfun', type=str, default=None,
-                        help='loss function? if None, then default bce for multi_labels, or default ce for multi_classes.'
-                             ' choose from: bce, ce, mse')
-
-    args = parser.parse_args()
-
+    # configuring the device
     if args.useGPU >= 0:
         # device = torch.device("cuda:{}".format(args.useGPU))
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
-
     # Set random seed for Pytorch and Numpy for reproducibility
     random.seed(args.seed)
     np.random.seed(args.seed)
@@ -138,80 +101,30 @@ if __name__ == "__main__":
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    exec('opt_dataset = {}_conf()'.format(args.dataset))  # updated on 20240701 for simplicity
-    # if args.dataset == "EDDFS_delN_ml":  # multi-label multi-disease cls without normal samples
-    #     opt_dataset = EDDFS_delN_ml_conf()
-    # elif args.dataset == "EDDFS_delMandN_mc":  # single-label multi-disease cls without normal samples
-    #     opt_dataset = EDDFS_delMandN_mc_conf()
-    # elif args.dataset == "EDDFS_amd":  # AMD grading
-    #     opt_dataset = EDDFS_amd_conf()
-    # elif args.dataset == "EDDFS_dr":  # DR grading
-    #     opt_dataset = EDDFS_dr_conf()
-    # elif args.dataset == "EDDFS_glaucoma":    # glaucoma identification
-    #     opt_dataset = EDDFS_glaucoma_conf()
-    # elif args.dataset == "EDDFS_myopia":      # pathological myopia identification
-    #     opt_dataset = EDDFS_myopia_conf()
-    # elif args.dataset == "EDDFS_rvo":  # RVO identification
-    #     opt_dataset = EDDFS_rvo_conf()
-    # elif args.dataset == "EDDFS_ls":  # Laser photocoagulation identification
-    #     opt_dataset = EDDFS_ls_conf()
-    # elif args.dataset == "EDDFS_hyper":  # hypertension retinopathy identification
-    #     opt_dataset = EDDFS_hyper_conf()
-    # elif args.dataset == "EDDFS_other":  # others or not identification
-    #     opt_dataset = EDDFS_other_conf()
-    # elif args.dataset == "ODIR_delMandN_mc":  # single-label multi-disease cls without normal samples
-    #     opt_dataset = ODIR_delMandN_mc_conf  # on OIA-ODIR dataset
-    # elif args.dataset == "APTOS2019":
-    #     opt_dataset = APTOS2019_conf
-    # else:
-    #     raise ValueError("args.dataset is not supported!!!")
-
-    classes_num = opt_dataset.classes_num
-    classes_names = opt_dataset.classes_names
-    image_root = opt_dataset.IMG_ROOT
-    label_dir = opt_dataset.LABEL_DIR
-    image_size = args.imagesize
-    net_name = args.net
-    batchsize = 8
-    loc = {"create_model": resnet18()}  # as a marker
-    glb = {}
-    exec("from models import {} as create_model".format(net_name), glb, loc)
-    create_model = loc["create_model"]
+    # creating model
+    module = importlib.import_module("models")  # Updated for simplicity and safety
+    create_model = getattr(module, args.net)
     model = create_model(num_classes=classes_num, pretrained=True)
 
-    resume = args.weight
-    if resume:
-        if os.path.isfile(resume):
-            print("=> resume is True. ====\n====loading checkpoint '{}'".format(resume))
-            checkpoint = torch.load(resume, map_location=device)  # or torch.load(resume)
-            start_epoch = checkpoint['epoch']
-            start_step = checkpoint['step']
-            model.load_state_dict(checkpoint['state_dict'])
-            # optimizer.load_state_dict(checkpoint['optimizer'])
-            # for state in optimizer.state.values():
-            #     for k, v in state.items():
-            #         if torch.is_tensor(v):
-            #             state[k] = v.to(device)
-            print('Model loaded from {}'.format(resume))
-        else:
-            raise ValueError(" ??? no checkpoint found at '{}'".format(resume))
+    if os.path.isfile(args.weight):
+        print("=> resume is True. ====\n====loading checkpoint '{}'".format(args.weight))
+        checkpoint = torch.load(args.weight, map_location=device)  # or torch.load(resume)
+        model.load_state_dict(checkpoint['state_dict'])
+        print('Model loaded from {}'.format(args.weight))
     else:
-        start_epoch = 0
-        start_step = 0
+        raise ValueError(" ??? no checkpoint found at '{}'".format(args.weight))
 
-    exec("from datasets import {} as dataset".format(args.dataset + "_Dataset"), glb, loc)
-    dataset = loc["dataset"]
     eval_dataset = dataset(image_root, label_dir, preprocess=str(args.preprocess),
                            meanbright=opt_dataset.MEAN_BRIGHTNESS, mask_path='mask.png',
                            phase="test",
                            transform=transforms.Compose([
-                               transforms.Resize(size=image_size)]), prepro_once=False,
+                               transforms.Resize(size=image_size)])
                            )
-    eval_loader = DataLoader(eval_dataset, batchsize, shuffle=False, num_workers=args.numworkers)
+    eval_loader = DataLoader(eval_dataset, args.batchsize, shuffle=False, num_workers=args.numworkers)
     # ===================
     # concat testing and training sets
     # concat_data = ConcatDataset([train_dataset, eval_dataset])
-    # concat_loader = DataLoader(concat_data, batchsize, shuffle=False, num_workers=args.numworkers)
+    # concat_loader = DataLoader(concat_data, args.batchsize, shuffle=False, num_workers=args.numworkers)
     # ===================
 
     if args.lossfun is None:
@@ -252,9 +165,54 @@ if __name__ == "__main__":
     df.to_csv(args.weight[:-8] + "-test-pred_cls_list.csv")
 
     # computing confusion matrix
-    # cm = confusion_matrix(labels_cls_list, pred_cls_list)
-    # plot_confusion_matrix(cm, classes_names, 'confusion_matrix.png', title='confusion matrix')
+    cm = confusion_matrix(labels_cls_list, pred_cls_list)
+    plot_confusion_matrix(cm, classes_names, 'confusion_matrix.png', title='confusion matrix')
 
     print("============ finished test==============")
     print()
     print("====================================")
+
+
+if __name__ == "__main__":
+    all_data_infor = ('EDDFS_delN_ml,'  # multi-label(ml) multi-disease without normal(delN)
+                      'EDDFS_delMandN_mc,'  # single-label multi-disease(mc) without normal(delMandN, 
+                                            # deleted multi-label and normal samples)
+                      'EDDFS_dr, EDDFS_amd,'  # DR and AMD grading
+                      'EDDFS_glaucoma, EDDFS_myopia, EDDFS_rvo,' # binary classification, 
+                      'EDDFS_ls, EDDFS_hyper, EDDFS_other,'  # but we marked them as multi_classes for simplicity
+                      'ODIR_delMandN_mc, APTOS2019')  # a multi-disease dataset ODIR, and a DR dataset APTOS
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--useGPU', type=int, default=0,
+                        help='-1: "cpu"; 0, 1, ...: "cuda:x";')
+    parser.add_argument('--seed', type=int, default=2022)
+
+    parser.add_argument('--dataset', type=str, default="EDDFS_delMandN_mc",
+                        help=all_data_infor)
+    parser.add_argument('--preprocess', type=str, default='6',
+                        help='preprocessing type')
+    parser.add_argument('--imagesize', type=int, default=448,
+                        help='image size')
+
+    parser.add_argument('--net', type=str, default='coattnet_v2_withWeighted_tiny',
+                        help='coattnet_v2_withWeighted_tiny' # ours
+                             'resnet18,'
+                             'resnext50_32x4d,'
+                             'densenet121,'
+                             'inception_v3,'
+                             'efficientnet_b2, efficientnetv2_s,'
+                             'dnn_18,')
+
+    parser.add_argument('--numworkers', type=int, default=4,  # 0 might be suitable for windows
+                        help='num_workers')
+    parser.add_argument('--batchsize', type=int, default=4,  # 0 might be suitable for windows
+                        help='batch_size')
+    parser.add_argument('--weight', type=str,
+                        default="weights/NC_delMandN_mc_7_448-parallelnet_v2_withWeighted_tiny_e51_bFalse_bs32-l9e-05_0.2-preFalse-lossce.pth.tar",
+                        help='load trained model?')
+    parser.add_argument('--lossfun', type=str, default=None,
+                        help='loss function? if None, then default bce for multi_labels, or default ce for multi_classes.'
+                             ' choose from: bce, ce, mse')
+    args = parser.parse_args()
+
+    main(args)
